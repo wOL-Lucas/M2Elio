40206cb22fcb6af06bcf54393d0e9a09d2491065
wOL-Lucas <lucasefernandes333@gmail.com>
2023-12-08 19:28:07 -0300

    Impl: implementing .properties file handling

--- LibraryManager/src/main/java/Database/connector.java
+++ LibraryManager/src/main/java/Database/connector.java
@@ -1,26 +1,42 @@
 package Database;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @NoArgsConstructor @AllArgsConstructor
 public class connector {
-    private String jdbcUrl = "jdbc:mysql://18.216.230.107:3306/library";
-    private String username = "eliosProject";
-    private String password = "professor";
 
+    Properties properties = loadPropertiesFile();
+
+    private String jdbcUrl = properties.getProperty("db.url");
+    private String username = properties.getProperty("db.username");
+    private String password = properties.getProperty("db.password");
 
     public Connection createConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(this.jdbcUrl,this.username,this.password);
+        Connection connection = DriverManager.getConnection(this.jdbcUrl, this.username, this.password);
         connection.setAutoCommit(true);
 
         return connection;
     }
 
+    private Properties loadPropertiesFile() {
+        Properties properties = new Properties();
+        try (InputStream input = connector.class.getClassLoader().getResourceAsStream(".properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find .properties");
+                return properties;
+            }
+            properties.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
 }
